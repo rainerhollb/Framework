26e2ea36832e65c3f641328841c2b18041a0c555
rainerhollb <49534931+rainerhollb@users.noreply.github.com>
2023-08-07 19:22:00 +0200

    + From XCode-Projects branchMain

--- Sources/Libs/Log.swift
+++ Sources/Libs/Log.swift
@@ -5,7 +5,44 @@
 //  Created by Rainer on 05.08.23.
 //
 
+// logging: https://developer.apple.com/documentation/os/logging/generating_log_messages_from_your_code
+// read: https://developer.apple.com/documentation/oslog
+
+/*
+ https://nshipster.com/swift-log/
+ =>
+ To be able to import Logging, add to Package.swift:
+ products: [
+ .executable(name: "audit", targets: ["audit"])
+ ],
+ dependencies: [
+ .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+ ],
+ targets: [
+ .target(name: "audit", dependencies: ["Logging"])
+ ]
+
+ or if not present add the whole file in the root folder of your project:
+ import PackageDescription
+ 
+ let package = Package(
+ name: "Auditor2000",
+ products: [
+ .executable(name: "audit", targets: ["audit"])
+ ],
+ dependencies: [
+ .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+ ],
+ targets: [
+ .target(name: "audit", dependencies: ["Logging"])
+ ]
+ )
+ 
+ .. not working
+ */
+
 import Foundation
+//import Logging
 
 /**
  Get the struct name by additional protocol.
@@ -22,6 +59,8 @@ extension Describable {
 
 struct Log {
    
+   //let DEFAULT_LOG = Logger()
+   
    /**
     To use origin, add Desribable protocol to the using struct and use Self.typeName as 2nd argument in this call.
     In static code, Self can be omitted.
@@ -32,6 +71,7 @@ struct Log {
          blank += " "
       }
       print(Time.timeString(Time.now(),Time.FORMAT_HH_MM_SS_MILLIS) + " " + origin! + blank + ": " + text)
+      //DEFAULT_LOG.info(origin! + blank + ": " + text)
    }
    
 }
