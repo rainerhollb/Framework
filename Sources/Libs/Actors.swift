a0786ee64cfe9a2ef76289a277434b85f61bec1c
rainerhollb <49534931+rainerhollb@users.noreply.github.com>
2023-08-16 21:19:45 +0200

    + Actors

---
+++ Sources/Libs/Actors.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by rainer on 16.08.23.
+//
+
+import Foundation
+
+
+actor TextActor {
+  var text : String
+  
+  init(_ text: String){
+    self.text = text
+  }
+  
+  func update(_ text: String) {
+    self.text = text
+  }
+  
+  func get() -> String {
+    return text
+  }
+}
+
+actor BoolActor {
+  var bool : Bool
+  
+  init(_ bool : Bool){
+    self.bool = bool
+  }
+  
+  func update(_ bool : Bool) {
+    self.bool = bool
+  }
+  
+  func get() -> Bool {
+    return bool
+  }
+}
