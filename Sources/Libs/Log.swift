23b88f635aae00695846aaeac6769600061cc329
rainerhollb <49534931+rainerhollb@users.noreply.github.com>
2023-08-28 22:52:21 +0200

    - removed simulator dependent disabled logging - removed filePath()

--- Sources/Libs/Log.swift
+++ Sources/Libs/Log.swift
@@ -38,7 +38,6 @@
  ]
  )
  
- .. not working
  */
 
 import Foundation
@@ -76,77 +75,73 @@ public class Log : Describable {
    let LOG_GROUP = DispatchGroup()
    var fileUpdater : FileHandle?
    
-   let SIMULATOR_USES_LOG = true
-   
    /**
     Caution: log files are causing crashes at seekEndOfFile even if logDeviceInfosInGroup is deactivated!
     Use filename only in test environment!
     If you need logging specify filename without path and without extension. If not set, only logging to console is possible.
     This path will result: <path to app documents> + filename + fileindex + ".txt"
+    
+    Experiences with sequencially written files:
+    
+    In simulators audio gets in endless loop, whole simulation running host (iMac) gets affected, also Music app!
+    In real devices app terminiates.
+    
+    Example:
+    
+    2023-08-25 16:49:37.500934+0200 Bottle Petrol Lamp[2327:30874] *** Terminating app due to uncaught exception 'NSFileHandleOperationException', reason: '*** -[NSConcreteFileHandle seekToEndOfFile]: unknown error'
+    *** First throw call stack:
+    (
+    0   CoreFoundation                      0x0000000180437330 __exceptionPreprocess + 172
+    1   libobjc.A.dylib                     0x0000000180051274 objc_exception_throw + 56
+    2   Foundation                          0x0000000180ae5fe0 -[NSConcreteFileHandle readDataUpToLength:error:] + 0
+    3   Foundation                          0x0000000180ae6654 -[NSConcreteFileHandle seekToEndReturningOffset:error:] + 0
+    4   Bottle Petrol Lamp                  0x00000001024b5584 $s18Bottle_Petrol_Lamp3LogC3log33_9C8E0856DEFDBF6431C601765D58F32ALLyySSF + 3276
+    5   Bottle Petrol Lamp                  0x00000001024b3b98 $s18Bottle_Petrol_Lamp3LogC5timedyySS_SStF + 156
+    6   Bottle Petrol Lamp                  0x00000001024aa89c $s18Bottle_Petrol_Lamp9WheelViewV25updateSelectedMobileLimityyYaFTY0_ + 320
+    7   Bottle Petrol Lamp                  0x00000001024aa4f5 $s18Bottle_Petrol_Lamp9WheelViewV4bodyQrvg7SwiftUI05TupleE0VyAE6VStackVyAE7SectionVyAE0E0PAEE8textCaseyQrAE4TextV0M0OSgFQOyAP_Qo_AmEE4task8priority_QrScP_yyYaYbctFQOyAmEE11pickerStyleyQrqd__AE06PickerR0Rd__lFQOyAE0S0VyAPSSAE7ForEachVySaySSGSSAPGG_AE0dsR0VQo__Qo_AE05EmptyE0VGG_A11_tGyXEfU0_A10_yXEfU0_A7_yXEfU_yyYaYbcfU0_TQ1_ + 1
+    8   Bottle Petrol Lamp                  0x00000001024ae1bd $s18Bottle_Petrol_Lamp9WheelViewV4bodyQrvg7SwiftUI05TupleE0VyAE6VStackVyAE7SectionVyAE0E0PAEE8textCaseyQrAE4TextV0M0OSgFQOyAP_Qo_AmEE4task8priority_QrScP_yyYaYbctFQOyAmEE11pickerStyleyQrqd__AE06PickerR0Rd__lFQOyAE0S0VyAPSSAE7ForEachVySaySSGSSAPGG_AE0dsR0VQo__Qo_AE05EmptyE0VGG_A11_tGyXEfU0_A10_yXEfU0_A7_yXEfU_yyYaYbcfU0_TATQ0_ + 1
+    9   SwiftUI                             0x000000010914f75d objectdestroy.330Tm + 2397
+    10  SwiftUI                             0x000000010914ef25 objectdestroy.330Tm + 293
+    11  SwiftUI                             0x00000001088e51b5 objectdestroy.30Tm + 2197
+    12  SwiftUI                             0x00000001088e51f5 objectdestroy.30Tm + 2261
+    13  libswift_Concurrency.dylib          0x00000001b1660445 _ZL23completeTaskWithClosurePN5swift12AsyncContextEPNS_10SwiftErrorE + 1
+    )
+    libc++abi: terminating due to uncaught exception of type NSException
+    
+    But also with disabled log files it crashes on a slightly different position and maybe after more run time:
+    
+    2023-08-25 18:27:29.986118+0200 Bottle Petrol Lamp[5581:96662] *** Terminating app due to uncaught exception 'NSFileHandleOperationException', reason: '*** -[NSConcreteFileHandle writeData:]: unknown error'
+    *** First throw call stack:
+    (
+    0   CoreFoundation                      0x0000000180437330 __exceptionPreprocess + 172
+    1   libobjc.A.dylib                     0x0000000180051274 objc_exception_throw + 56
+    2   Foundation                          0x0000000180ae5fe0 -[NSConcreteFileHandle readDataUpToLength:error:] + 0
+    3   Foundation                          0x0000000180ae62d8 -[NSConcreteFileHandle writeData:] + 256
+    4   Bottle Petrol Lamp                  0x0000000102b255f0 $s18Bottle_Petrol_Lamp3LogC3log33_9C8E0856DEFDBF6431C601765D58F32ALLyySSF + 4084
+    5   Bottle Petrol Lamp                  0x0000000102b238fc $s18Bottle_Petrol_Lamp3LogC5timedyySS_SStF + 156
+    6   Bottle Petrol Lamp                  0x0000000102b449f4 $s18Bottle_Petrol_Lamp11ContentViewV25updateRadioStationDisplayyyYaFTY0_ + 176
+    7   Bottle Petrol Lamp                  0x0000000102b446b1 $s18Bottle_Petrol_Lamp11ContentViewV4bodyQrvg7SwiftUI05TupleE0VyAE5GroupVyAGyAE0E0PAEE4fontyQrAE4FontVSgFQOyAE6ButtonVyAE4TextVG_Qo__AkEE4task8priority_QrScP_yyYaYbctFQOyAS_Qo_AsE6SliderVyAE05EmptyE0VA0_GtGG_AIyAGyAU_AkEEAvW_QrScP_yyYaYbctFQOyAkEE09multilineN9AlignmentyQrAE0nT0OFQOyAS_Qo__Qo_tGGAkEEAvW_QrScP_yyYaYbctFQOyAkEEA4_yQrA6_FQOyAkEE5frame8minWidth05idealW003maxW00V6Height0xZ00yZ09alignmentQr12CoreGraphics7CGFloatVSg_A22_A22_A22_A22_A22_AE0T0VtFQOyAS_Qo__Qo__Qo_tGyXEfU0_A9_yXEfU0_yyYaYbcfU_TQ1_ + 1
+    8   Bottle Petrol Lamp                  0x0000000102b4d15d $s18Bottle_Petrol_Lamp11ContentViewV4bodyQrvg7SwiftUI05TupleE0VyAE5GroupVyAGyAE0E0PAEE4fontyQrAE4FontVSgFQOyAE6ButtonVyAE4TextVG_Qo__AkEE4task8priority_QrScP_yyYaYbctFQOyAS_Qo_AsE6SliderVyAE05EmptyE0VA0_GtGG_AIyAGyAU_AkEEAvW_QrScP_yyYaYbctFQOyAkEE09multilineN9AlignmentyQrAE0nT0OFQOyAS_Qo__Qo_tGGAkEEAvW_QrScP_yyYaYbctFQOyAkEEA4_yQrA6_FQOyAkEE5frame8minWidth05idealW003maxW00V6Height0xZ00yZ09alignmentQr12CoreGraphics7CGFloatVSg_A22_A22_A22_A22_A22_AE0T0VtFQOyAS_Qo__Qo__Qo_tGyXEfU0_A9_yXEfU0_yyYaYbcfU_TATQ0_ + 1
+    9   SwiftUI                             0x000000010989375d objectdestroy.330Tm + 2397
+    10  SwiftUI                             0x0000000109892f25 objectdestroy.330Tm + 293
+    11  SwiftUI                             0x00000001090291b5 objectdestroy.30Tm + 2197
+    12  SwiftUI                             0x00000001090291f5 objectdestroy.30Tm + 2261
+    13  libswift_Concurrency.dylib          0x00000001b1660445 _ZL23completeTaskWithClosurePN5swift12AsyncContextEPNS_10SwiftErrorE + 1
+    )
+    libc++abi: terminating due to uncaught exception of type NSException
     */
    public init(
       _ logToConsole: Bool = true,
       _ filename: String? = nil,
-      isErrorLog : Bool = false)
-   {
-      self.logToConsole = logToConsole
-      if SIMULATOR_USES_LOG || !Device.IS_SIMULATOR {
-         print("This device is categorized as no simulator. Logs are created.");
+      isErrorLog : Bool = false) {
+         self.logToConsole = logToConsole
+         
          self.filename = filename
          if filename != nil && !isErrorLog {
             errorLog = Log(false, filename! + "Error", isErrorLog: true)
          }
-      } else {
-         print("This device is categorized as simulator. Logs may cause instability and are not set up.");
-         /*
-          Experiences:
-          
-          Audio gets in endless loop, whole simulation running host (iMac) gets affected, also Music app!
-          
-          2023-08-25 16:49:37.500934+0200 Bottle Petrol Lamp[2327:30874] *** Terminating app due to uncaught exception 'NSFileHandleOperationException', reason: '*** -[NSConcreteFileHandle seekToEndOfFile]: unknown error'
-          *** First throw call stack:
-          (
-          0   CoreFoundation                      0x0000000180437330 __exceptionPreprocess + 172
-          1   libobjc.A.dylib                     0x0000000180051274 objc_exception_throw + 56
-          2   Foundation                          0x0000000180ae5fe0 -[NSConcreteFileHandle readDataUpToLength:error:] + 0
-          3   Foundation                          0x0000000180ae6654 -[NSConcreteFileHandle seekToEndReturningOffset:error:] + 0
-          4   Bottle Petrol Lamp                  0x00000001024b5584 $s18Bottle_Petrol_Lamp3LogC3log33_9C8E0856DEFDBF6431C601765D58F32ALLyySSF + 3276
-          5   Bottle Petrol Lamp                  0x00000001024b3b98 $s18Bottle_Petrol_Lamp3LogC5timedyySS_SStF + 156
-          6   Bottle Petrol Lamp                  0x00000001024aa89c $s18Bottle_Petrol_Lamp9WheelViewV25updateSelectedMobileLimityyYaFTY0_ + 320
-          7   Bottle Petrol Lamp                  0x00000001024aa4f5 $s18Bottle_Petrol_Lamp9WheelViewV4bodyQrvg7SwiftUI05TupleE0VyAE6VStackVyAE7SectionVyAE0E0PAEE8textCaseyQrAE4TextV0M0OSgFQOyAP_Qo_AmEE4task8priority_QrScP_yyYaYbctFQOyAmEE11pickerStyleyQrqd__AE06PickerR0Rd__lFQOyAE0S0VyAPSSAE7ForEachVySaySSGSSAPGG_AE0dsR0VQo__Qo_AE05EmptyE0VGG_A11_tGyXEfU0_A10_yXEfU0_A7_yXEfU_yyYaYbcfU0_TQ1_ + 1
-          8   Bottle Petrol Lamp                  0x00000001024ae1bd $s18Bottle_Petrol_Lamp9WheelViewV4bodyQrvg7SwiftUI05TupleE0VyAE6VStackVyAE7SectionVyAE0E0PAEE8textCaseyQrAE4TextV0M0OSgFQOyAP_Qo_AmEE4task8priority_QrScP_yyYaYbctFQOyAmEE11pickerStyleyQrqd__AE06PickerR0Rd__lFQOyAE0S0VyAPSSAE7ForEachVySaySSGSSAPGG_AE0dsR0VQo__Qo_AE05EmptyE0VGG_A11_tGyXEfU0_A10_yXEfU0_A7_yXEfU_yyYaYbcfU0_TATQ0_ + 1
-          9   SwiftUI                             0x000000010914f75d objectdestroy.330Tm + 2397
-          10  SwiftUI                             0x000000010914ef25 objectdestroy.330Tm + 293
-          11  SwiftUI                             0x00000001088e51b5 objectdestroy.30Tm + 2197
-          12  SwiftUI                             0x00000001088e51f5 objectdestroy.30Tm + 2261
-          13  libswift_Concurrency.dylib          0x00000001b1660445 _ZL23completeTaskWithClosurePN5swift12AsyncContextEPNS_10SwiftErrorE + 1
-          )
-          libc++abi: terminating due to uncaught exception of type NSException
-          
-          But also with disabled log files it crashes on a slightly different position and maybe after more run time:
-          
-          2023-08-25 18:27:29.986118+0200 Bottle Petrol Lamp[5581:96662] *** Terminating app due to uncaught exception 'NSFileHandleOperationException', reason: '*** -[NSConcreteFileHandle writeData:]: unknown error'
-          *** First throw call stack:
-          (
-          0   CoreFoundation                      0x0000000180437330 __exceptionPreprocess + 172
-          1   libobjc.A.dylib                     0x0000000180051274 objc_exception_throw + 56
-          2   Foundation                          0x0000000180ae5fe0 -[NSConcreteFileHandle readDataUpToLength:error:] + 0
-          3   Foundation                          0x0000000180ae62d8 -[NSConcreteFileHandle writeData:] + 256
-          4   Bottle Petrol Lamp                  0x0000000102b255f0 $s18Bottle_Petrol_Lamp3LogC3log33_9C8E0856DEFDBF6431C601765D58F32ALLyySSF + 4084
-          5   Bottle Petrol Lamp                  0x0000000102b238fc $s18Bottle_Petrol_Lamp3LogC5timedyySS_SStF + 156
-          6   Bottle Petrol Lamp                  0x0000000102b449f4 $s18Bottle_Petrol_Lamp11ContentViewV25updateRadioStationDisplayyyYaFTY0_ + 176
-          7   Bottle Petrol Lamp                  0x0000000102b446b1 $s18Bottle_Petrol_Lamp11ContentViewV4bodyQrvg7SwiftUI05TupleE0VyAE5GroupVyAGyAE0E0PAEE4fontyQrAE4FontVSgFQOyAE6ButtonVyAE4TextVG_Qo__AkEE4task8priority_QrScP_yyYaYbctFQOyAS_Qo_AsE6SliderVyAE05EmptyE0VA0_GtGG_AIyAGyAU_AkEEAvW_QrScP_yyYaYbctFQOyAkEE09multilineN9AlignmentyQrAE0nT0OFQOyAS_Qo__Qo_tGGAkEEAvW_QrScP_yyYaYbctFQOyAkEEA4_yQrA6_FQOyAkEE5frame8minWidth05idealW003maxW00V6Height0xZ00yZ09alignmentQr12CoreGraphics7CGFloatVSg_A22_A22_A22_A22_A22_AE0T0VtFQOyAS_Qo__Qo__Qo_tGyXEfU0_A9_yXEfU0_yyYaYbcfU_TQ1_ + 1
-          8   Bottle Petrol Lamp                  0x0000000102b4d15d $s18Bottle_Petrol_Lamp11ContentViewV4bodyQrvg7SwiftUI05TupleE0VyAE5GroupVyAGyAE0E0PAEE4fontyQrAE4FontVSgFQOyAE6ButtonVyAE4TextVG_Qo__AkEE4task8priority_QrScP_yyYaYbctFQOyAS_Qo_AsE6SliderVyAE05EmptyE0VA0_GtGG_AIyAGyAU_AkEEAvW_QrScP_yyYaYbctFQOyAkEE09multilineN9AlignmentyQrAE0nT0OFQOyAS_Qo__Qo_tGGAkEEAvW_QrScP_yyYaYbctFQOyAkEEA4_yQrA6_FQOyAkEE5frame8minWidth05idealW003maxW00V6Height0xZ00yZ09alignmentQr12CoreGraphics7CGFloatVSg_A22_A22_A22_A22_A22_AE0T0VtFQOyAS_Qo__Qo__Qo_tGyXEfU0_A9_yXEfU0_yyYaYbcfU_TATQ0_ + 1
-          9   SwiftUI                             0x000000010989375d objectdestroy.330Tm + 2397
-          10  SwiftUI                             0x0000000109892f25 objectdestroy.330Tm + 293
-          11  SwiftUI                             0x00000001090291b5 objectdestroy.30Tm + 2197
-          12  SwiftUI                             0x00000001090291f5 objectdestroy.30Tm + 2261
-          13  libswift_Concurrency.dylib          0x00000001b1660445 _ZL23completeTaskWithClosurePN5swift12AsyncContextEPNS_10SwiftErrorE + 1
-          )
-          libc++abi: terminating due to uncaught exception of type NSException
-          */
-      }
    }
+   
    public func logDeviceInfos( _ loggingClass: String) {
       LOG_GROUP.enter()
       logDeviceInfosInGroup(loggingClass)
@@ -166,19 +161,6 @@ public class Log : Describable {
       log("*********************************************************************")
       errorLog?.log(FORMATTED_TEXT)
    }
-   
-   /*
-    fileprivate func filePath() -> String? {
-    if filename == nil {
-    return nil
-    }
-    if Log.APP_SANDBOX == nil {
-    return nil
-    }
-    
-    return Log.APP_SANDBOX!.path + "/" + filename! + String(fileIndex) + ".txt"
-    }
-    */
    
    fileprivate func fileURL() -> URL? {
       if filename == nil {
